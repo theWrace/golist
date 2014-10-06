b036620a3418c10257bf3c372d150c6aaf836254
daniel <danielg17903@web.de>
2014-10-06 13:50:09 +0200

    fehler behoben

--- GoList/src/da/se/golist/Login.java
+++ GoList/src/da/se/golist/Login.java
@@ -1,6 +1,8 @@
+package da.se.golist;
 import da.se.golist.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 
 public class Login extends Activity{
@@ -8,6 +10,7 @@ public class Login extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.login);
 	}
 
