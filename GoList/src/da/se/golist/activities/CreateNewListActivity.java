26a1dac10b0daaa954f86cc9db2d52047ad48eb7
daniel <danielg17903@web.de>
2014-11-27 13:12:23 +0100

    test

--- GoList/src/da/se/golist/activities/CreateNewListActivity.java
+++ GoList/src/da/se/golist/activities/CreateNewListActivity.java
@@ -25,7 +25,6 @@ import da.se.golist.objects.User;
 
 public class CreateNewListActivity extends DataLoader{
 	
-	//test
 	private ArrayList<GoListObject> userOfList = new ArrayList<GoListObject>();
 	private UserListAdapter listAdapter;
 	private Button buttonSave;
